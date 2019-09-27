4a6ec381a5a33bd2ecba83750aaba8f480312f33
Vishal Kumar Singh <01vksingh@gmail.com>
2019-09-27 23:45:39 +0530

    added notification to payment

--- app/src/main/java/com/example/food_app/pay.java
+++ app/src/main/java/com/example/food_app/pay.java
@@ -1,14 +1,25 @@
 package com.example.food_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class pay extends AppCompatActivity {
+
+    private final String CHANNEL_ID = "personal_notification";
+    private final String channel_name="test";
+
+    private final int NOTIFICATION_ID=001;
 
     TextView yt;
     String overall;
@@ -31,6 +42,22 @@ public class pay extends AppCompatActivity {
         ut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,channel_name, NotificationManager.IMPORTANCE_HIGH);
+                notificationChannel.enableLights(true);
+                notificationChannel.enableVibration(true);
+                notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager.createNotificationChannel(notificationChannel);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),CHANNEL_ID);
+                builder.setSmallIcon(R.drawable.logovk1);
+                builder.setContentTitle("food app");
+                builder.setContentText("your order is getting prepared and will be soon delivered");
+                builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
+                notificationManagerCompat.notify(NOTIFICATION_ID,builder.build());
+                Toast.makeText(getApplicationContext(),"payment",Toast.LENGTH_SHORT).show();
 
                 Intent go = new Intent(getApplicationContext(),loggedin.class);
                 startActivity(go);
